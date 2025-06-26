8b3d9235e89c7b3109284b10071c81d1f1973365
Emil Salagean <emil.salageanu>
2025-06-26 10:14:27 +0200

    mior fixes

--- data_updater/google_utils.py
+++ data_updater/google_utils.py
@@ -7,7 +7,7 @@ def get_spreadsheet():
     """Get the main Google Spreadsheet using credentials and URL from environment variables"""
     scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
 
-    credentials = Credentials.from_service_account_file('poly-maker-google-sheets-credentials.json', scopes=scope)
+    credentials = Credentials.from_service_account_file('credentials.json', scopes=scope)
     client = gspread.authorize(credentials)
     spreadsheet = client.open_by_url(os.getenv("SPREADSHEET_URL"))
     return spreadsheet
